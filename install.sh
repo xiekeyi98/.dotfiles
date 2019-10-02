2225862aefcaa8fc80149a135785d8c42c583ac3
keyixie <keyixie@tencent.com>
2019-10-02 19:40:34 +0800

    No need powerlever9k
    Signed-off-by: keyixie <keyixie@tencent.com>

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ ln -s ~/.dotfiles/zsh/oh-my-zsh/ ~/.oh-my-zsh # install oh-my-zsh
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+echo "Install font please."
+# git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 chsh -s $(which zsh)
 
