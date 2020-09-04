6a905b8d40d0b4ad4e8a04a2108b04b66cc26a17
keyixie <keyixie@tencent.com>
2020-09-04 12:35:40 +0800

    update

--- install.sh
+++ install.sh
@@ -18,6 +18,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # 安装 pwerleverl10k 插件
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 echo "Install font please."
+# 安装 zsh-syntax-hightlight
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 和 autosugg 兼容不好
 # 应该不需要，10k 应该可以兼容
 # git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k 
 
@@ -30,6 +32,7 @@ echo "install screenfetch"
 echo "install lshw"
 echo "install git cz"
 echo "install git  cz-emoji"
+echo "install mycli"
 # echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
 # echo '{ "path": "cz-emoji" }' > ~/.czrc
 
@@ -38,6 +41,7 @@ echo "install git  cz-emoji"
 ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global #把 git 全局忽略的文件配过去
 ln -s ~/.dotfiles/git/gitmessage ~/.gitmessage #把 git 全局忽略的文件配过去
 # ln -s ~/.dotfiles/config/ssh ~/.ssh
+
 
 
 # for git
